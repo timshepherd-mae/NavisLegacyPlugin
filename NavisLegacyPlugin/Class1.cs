7e73c407ef9304fa63224f55ae179371dd205575
tshepherd <tshepherd@PF3KN9LB>
2026-04-23 13:58:36 +0100

    Add project files.

---
+++ NavisLegacyPlugin/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+using NvApp = Autodesk.Navisworks.Api.Application;
+
+namespace NavisLegacyPlugin
+{
+	[PluginAttribute("NavisLegacyPlugin", "TEST", DisplayName = "Legacy Msg", ToolTip = "4.8 Framework Plugin")]
+	[AddInPluginAttribute(AddInLocation.AddIn)]
+
+	public class MainEntryPoint : AddInPlugin
+
+	{
+		public override int Execute(params string[] parameters)
+
+		{
+			var wnd = new MainWindow();
+			NavisworksWindowHelper.SetOwner(wnd);
+			wnd.Show();
+			wnd.Activate();
+			return 0;
+		}
+	}
+
+}
---
+++ NavisLegacyPlugin/NavisworksWindowHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using Autodesk.Navisworks.Api;
+
+namespace NavisLegacyPlugin
+{
+	internal static class NavisworksWindowHelper
+	{
+		public static void SetOwner(Window window)
+		{
+			IntPtr navisHandle =
+				Autodesk.Navisworks.Api.Application.Gui.MainWindow.Handle;
+
+			new WindowInteropHelper(window)
+			{
+				Owner = navisHandle
+			};
+		}
+	}
+}
