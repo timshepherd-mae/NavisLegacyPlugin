34763d0889fcb6909425ec1885f93bf2b94cfd35
timshepherd-mae <timshepherd@murphygroup.co.uk>
2026-04-23 17:16:17 +0100

    reset

---
+++ NavisLegacyPlugin/EntryPoint/MainEntryPointLauncher.cs
@@ -0,0 +1,27 @@
+﻿namespace NavisLegacyPlugin
+{
+	internal static class MainEntryPointLauncher
+	{
+		private static MainWindow _window;
+
+		public static int Run()
+		{
+			if (_window == null || !_window.IsLoaded)
+			{
+				var contextService = new NavisworksContextService();
+
+				_window = new MainWindow(contextService);
+				NavisworksWindowHelper.SetOwner(_window);
+
+				_window.Closed += (_, __) => _window = null;
+				_window.ShowDialog();
+			}
+			else
+			{
+				_window.Activate();
+			}
+
+			return 0;
+		}
+	}
+}
