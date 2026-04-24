bf894417ad7b3825ffffa4d023bded7e0006014c
timshepherd-mae <timshepherd@murphygroup.co.uk>
2026-04-24 15:29:44 +0100

    #

---
+++ NavisLegacyPlugin/Services/NavisworksViewService.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Navisworks.Api;
+
+namespace NavisLegacyPlugin.Services
+{
+	public class NavisworksViewService
+	{
+		public void SelectAndZoomTo(ModelItem item)
+		{
+			if (item == null)
+				return;
+
+			var doc = Application.ActiveDocument;
+			if (doc == null)
+				return;
+
+			doc.CurrentSelection.Clear();
+			doc.CurrentSelection.Add(item);
+
+			// This exists in all Navisworks versions
+			doc.ActiveView.RequestFocus();
+		}
+	}
+}
