ea3cf36157d09d408134613a7c1a1b513b72d584
timshepherd-mae <timshepherd@murphygroup.co.uk>
2026-05-21 15:59:40 +0100

    created 3 tab user-controls, main-window == placeholder for tab-control

---
+++ NavisLegacyPlugin/UI/GeometryCompareTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Windows.Controls;
+
+
+
+namespace NavisLegacyPlugin.UI
+{
+	public partial class GeometryCompareTab : UserControl
+	{
+		public GeometryCompareTab()
+		{
+			InitializeComponent();
+		}
+	}
+}
