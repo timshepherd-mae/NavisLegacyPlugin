fff84c5f6c53bc60c831619ea8403dbff92ff882
timshepherd-mae <timshepherd@murphygroup.co.uk>
2026-05-21 16:44:24 +0100

    final user-tab refactors

---
+++ NavisLegacyPlugin/ViewModels/GeometryCompareViewModel.cs
@@ -0,0 +1,38 @@
+﻿using NavisLegacyPlugin.Services;
+using System.Windows.Input;
+
+namespace NavisLegacyPlugin.ViewModels
+{
+	public class GeometryCompareViewModel : ViewModelBase
+	{
+		private readonly NavisworksContextService _contextService;
+
+		public GetGeometryPositionsViewModel GeometryA { get; }
+		public GetGeometryPositionsViewModel GeometryB { get; }
+
+		public ICommand ActionOneCommand { get; }
+		public ICommand ActionTwoCommand { get; }
+
+		public GeometryCompareViewModel(NavisworksContextService contextService)
+		{
+			_contextService = contextService;
+
+			var geometryService = new GeometryPositionService();
+			GeometryA = new GetGeometryPositionsViewModel(geometryService);
+			GeometryB = new GetGeometryPositionsViewModel(geometryService);
+
+			ActionOneCommand = new RelayCommand(OnActionOne);
+			ActionTwoCommand = new RelayCommand(OnActionTwo);
+		}
+
+		private void OnActionOne()
+		{
+			System.Diagnostics.Debug.WriteLine("Geometry Compare – Action One");
+		}
+
+		private void OnActionTwo()
+		{
+			System.Diagnostics.Debug.WriteLine("Geometry Compare – Action Two");
+		}
+	}
+}
--- NavisLegacyPlugin/ViewModels/MainWindowViewModel.cs
+++ NavisLegacyPlugin/ViewModels/MainWindowViewModel.cs
@@ -1,77 +1,14 @@
-﻿using System.Windows;
-using System.Windows.Input;
-using NavisLegacyPlugin.Services;
-using NavisLegacyPlugin;
+﻿using NavisLegacyPlugin.Services;
 
 namespace NavisLegacyPlugin.ViewModels
 {
 	public class MainWindowViewModel : ViewModelBase
 	{
-		private readonly NavisworksContextService _contextService;
-
-		private string _statusText;
-		public string StatusText
-		{
-			get => _statusText;
-			private set
-			{
-				_statusText = value;
-				OnPropertyChanged();
-			}
-		}
-
-		public ICommand ActionOneCommand { get; }
-		public ICommand ActionTwoCommand { get; }
-		public GetGeometryPositionsViewModel GeometryA { get; }
-		public GetGeometryPositionsViewModel GeometryB { get; }
+		public GeometryCompareViewModel GeometryCompare { get; }
 
 		public MainWindowViewModel(NavisworksContextService contextService)
 		{
-			_contextService = contextService;
-			LoadContext();
-
-			ActionOneCommand = new RelayCommand(OnActionOne);
-			ActionTwoCommand = new RelayCommand(OnActionTwo);
-
-			var geometryService = new GeometryPositionService();
-
-			GeometryA = new GetGeometryPositionsViewModel(geometryService);
-			GeometryB = new GetGeometryPositionsViewModel(geometryService);	
+			GeometryCompare = new GeometryCompareViewModel(contextService);
 		}
-
-		private void LoadContext()
-		{
-			if (!_contextService.HasActiveDocument)
-			{
-				StatusText = "No active Navisworks document.";
-				return;
-			}
-
-			var selection = _contextService.GetSelectionInfo();
-			StatusText = $"Selected items: {selection.SelectedItemCount}";
-		}
-
-		private void OnActionOne()
-		{
-			MessageBox.Show(
-				"Action One executed.",
-				"Action One",
-				MessageBoxButton.OK,
-				MessageBoxImage.Information);
-
-			System.Diagnostics.Debug.WriteLine("btn 1");
-		}
-
-		private void OnActionTwo()
-		{
-			MessageBox.Show(
-				"Action Two executed.",
-				"Action Two",
-				MessageBoxButton.OK,
-				MessageBoxImage.Information);
-		}
-
-
 	}
-
 }
